7b03911731c3b4b459d0b90ec3cf56f5e256393c
Lucas Geese <geeselucas@gmail.com>
2025-07-08 17:06:00 +0100

    "Update"

--- scripts/03_validation_and_robustness.R
+++ scripts/03_validation_and_robustness.R
@@ -1,3 +1,64 @@
+# ------------------------------------------------------------------------------
+# Script: 03_validation_and_robustness.R
+# Purpose: Provide validation for computational text analysis and robustness
+#          checks for statistical analysis
+# Author: Dr. Lucas Geese
+# Last updated: 2025-07-07
+# ------------------------------------------------------------------------------
+
+
+# Load required packages --------------------------------------------------
+
+# Define required packages
+required_packages <- c("here", "tidyverse", "sjmisc", "stringi", "stm", "quanteda", 
+                       "rvest", "devtools", "officer", "plm", "readxl", "vdemdata",
+                       "broom", "cowplot")
+
+# Install required packages only if not already installed (for reproducibility)
+# Only install packages if the script is run interactively (e.g., in an R session)
+# This avoids installation attempts during batch processing or when the script is executed
+# in a non-interactive environment (e.g. Rscript calls).
+if (interactive()) {
+  for (pkg in required_packages) {
+    if (!requireNamespace(pkg, quietly = TRUE)) {
+      install.packages(pkg)
+    }
+  }
+  # Install GitHub package if not already installed
+  if (!requireNamespace("vdemdata", quietly = TRUE)) {
+    remotes::install_github("vdeminstitute/vdemdata")
+  }
+}
+
+# Load libraries
+library(here)
+library(tidyverse)
+library(sjmisc)
+library(stringi)
+library(stm)
+library(quanteda)
+library(rvest)
+library(vdemdata)
+library(officer)
+library(plm)
+library(readxl)
+library(vdemdata)
+library(broom)
+library(cowplot)
+
+# Step 1: Assess alternative STM specifications ---------------------------
+
+load(here("models", "stm.fit_cclw.RData"))
+
+stm.fit_cclw40 <- stm(dtm_cclw_trimmed$documents, dtm_cclw_trimmed$vocab, K=40, 
+                     prevalence =~ HDI_Group_2023 + s(Year) + gdp_pc + v2x_gender + v2x_corr, 
+                     data = dtm_cclw_trimmed$meta, 
+                     init.type = "Spectral")
+
+stm.fit_cclw60 <- stm(dtm_cclw_trimmed$documents, dtm_cclw_trimmed$vocab, K=40, 
+                      prevalence =~ HDI_Group_2023 + s(Year) + gdp_pc + v2x_gender + v2x_corr, 
+                      data = dtm_cclw_trimmed$meta, 
+                      init.type = "Spectral")
 
 
 # Step 3: Import and prepare health outcome data --------------------------
